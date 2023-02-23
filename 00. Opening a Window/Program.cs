b186a1cb3c879ddc00edb08be90e37ddf38839b2
ZaBlazzingZephyrus <blazerzeif@gmail.com>
2023-02-23 18:57:49 +0700

    Add Lesson 00

---
+++ 00. Opening a Window/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Silk.NET.Windowing;
+using Silk.NET.Maths;
+namespace Lesson;
+
+internal class Program
+{
+    private static IWindow? window;
+
+    static void Main()
+    {
+        
+        var options = WindowOptions.Default with
+        {
+            API = GraphicsAPI.None,             // <-- This is important as
+                                                // the window is handled by OpenGL
+                                                // by default
+            Size = new Vector2D<int>(800, 800),
+            Title = "00. Opening a Window"
+        };
+
+        Program.window = Window.Create(options);
+        Program.window.Run();
+    }
+}
